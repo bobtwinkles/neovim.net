e9161210cfba1225bac2161994ab147d6a060e87
bobtwinkles <srkoser+GitHub@gmail.com>
2015-03-15 16:29:15 -0400

    initial commit

---
+++ NeovimIntegration/Properties/AddinInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Mono.Addins;
+using Mono.Addins.Description;
+
+[assembly:Addin (
+	"NeovimIntegration", 
+	Namespace = "NeovimIntegration",
+	Version = "1.0"
+)]
+
+[assembly:AddinName ("NeovimIntegration")]
+[assembly:AddinCategory ("NeovimIntegration")]
+[assembly:AddinDescription ("NeovimIntegration")]
+[assembly:AddinAuthor ("bob_twinkles")]
